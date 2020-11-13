4de982d5a825a3ec27d2220737dcc24a8513ac35
SergeyGolenko <1989sergiotay@gmail.com>
2020-11-13 18:19:07 +0200

    Initial Commit

---
+++ MiniApp(guess the number)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniApp(guess the number)
+//
+//  Created by Сергей Голенко on 13.11.2020.
+//  Copyright © 2020 Сергей Голенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
